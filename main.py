560373cca45e924cf3ddbf8e3d39bffa06b2a5d3
kerzhao <kerzhao@163.com>
2018-07-17 15:11:45 +0800

    udpate

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from multiprocessing import Process, Queue
 import os
+import time
 import argparse
-from vgg16_worker import Vgg16Worker
+from vgg16_worker import Vgg16Worker, Vgg16WorkerPool
+from vgg16_worker import set_global_xnet, predict
 
 
 class Scheduler:
@@ -34,6 +36,39 @@ class Scheduler:
         for worker in self._workers:
             worker.join()
         print "all of workers have been done"
+        
+class SchedulerPool(object):
+    def __init__(self, gpuids):
+        self._queue = Queue()
+        self._data = []
+        self._gpuids = gpuids
+        
+        self.p = None
+        self.__init_workers()
+
+    def __init_workers(self):
+        self._workers = list()
+        for gpuid in self._gpuids:
+            self._queue.put(gpuid)
+        self.p = Vgg16WorkerPool(len(self._gpuids), set_global_xnet, [self._queue])
+
+
+    def start(self, xfilelst):
+
+        # put all of files into queue
+        for xfile in xfilelst:
+            self._data.append(xfile)
+        
+        st = time.time()
+        r1 = self.p.map(predict, self._data)
+        #self.p.close()
+        #self.p.join()
+        time.sleep(20)
+        r2 = self.p.map(predict, self._data)
+        self.p.close()
+        self.p.join()
+        et = time.time()
+        print "all of workers have been done"
 
                 
 
@@ -41,10 +76,11 @@ def run(img_path, gpuids):
     #scan all files under img_path
     xlist = list()
     for xfile in os.listdir(img_path):
-        xlist.append(os.path.join(img_path, xfile))
+        if '.jpg' in xfile:
+            xlist.append(os.path.join(img_path, xfile))
     
     #init scheduler
-    x = Scheduler(gpuids)
+    x = SchedulerPool(gpuids)
     
     #start processing and wait for complete 
     x.start(xlist)
--- vgg16_worker.py
+++ vgg16_worker.py
@@ -1,7 +1,57 @@
 from multiprocessing import Queue, Process
+from multiprocessing.pool import Pool
 import cv2
 import numpy as np
 import os
+
+xnet = None
+
+def set_global_xnet(gpuids):
+    #from wingdebugger import wingdbstub
+    #wingdbstub.Ensure()
+    #wingdbstub.debugger.StartDebug()
+    global xnet
+    gpuid = gpuids.get()
+    print('#############################', gpuids)
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(gpuid)
+    import vgg16
+    xnet = vgg16.Vgg16('/media/zhaoke/b0685ee4-63e3-4691-ae02-feceacff6996/models/vgg16_weights_tf_dim_ordering_tf_kernels.h5')
+    #wingdbstub.debugger.StopDebug()
+    
+def predict(imgfile):
+    #BGR
+    im = cv2.resize(cv2.imread(imgfile), (224, 224)).astype(np.float32)
+
+    im[:, :, 0] -= 103.939
+    im[:, :, 1] -= 116.779
+    im[:, :, 2] -= 123.68
+
+    im = im.reshape((1, 224, 224, 3))
+    out = xnet.predict(im)
+    print ' xfile ', imgfile#, " predicted as label", out
+    return np.argmax(out)   
+    
+RUN = 0
+class Vgg16WorkerPool(Pool):
+    def __init__(self, processes, initializer=set_global_xnet, initargs=None):
+        super(Vgg16WorkerPool, self).__init__(processes=processes,
+                                              initializer=initializer,
+                                              initargs=initargs)
+        
+    def map(self, func, iterable, chunksize=None):
+        '''
+        Equivalent of `map()` builtin
+        '''
+        #from wingdebugger import wingdbstub
+        #wingdbstub.Ensure()
+        #wingdbstub.debugger.StartDebug()
+        assert self._state == RUN
+        ret = self.map_async(func, iterable, chunksize).get()
+        #wingdbstub.debugger.StopDebug()
+        return ret
+
+        
 
 class Vgg16Worker(Process):
     def __init__(self, gpuid, queue):
@@ -16,10 +66,13 @@ class Vgg16Worker(Process):
         os.environ["CUDA_VISIBLE_DEVICES"] = str(self._gpuid)
 
         #load models
+        import wingdbstub
+        wingdbstub.Ensure()
+        wingdbstub.debugger.StartDebug()
         import vgg16
         #download the vgg16 weights from
         #https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels.h5
-        xnet = vgg16.Vgg16('vgg16_weights_tf_dim_ordering_tf_kernels.h5')
+        xnet = vgg16.Vgg16('/media/zhaoke/b0685ee4-63e3-4691-ae02-feceacff6996/models/vgg16_weights_tf_dim_ordering_tf_kernels.h5')
 
         print 'vggnet init done', self._gpuid
 
@@ -32,6 +85,7 @@ class Vgg16Worker(Process):
             print 'woker', self._gpuid, ' xfile ', xfile, " predicted as label", label
 
         print 'vggnet done ', self._gpuid
+        wingdbstub.debugger.StopDebug()
 
     def predict(self, xnet, imgfile):
         #BGR
